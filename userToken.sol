df96c77c4ae43eeb73e77b5bc976c653b510fbab
sravya naguboyina <sravs.munny@gmail.com>
2018-01-27 23:34:44 +0530

    Updated the file userToken.sol

--- userToken.sol
+++ userToken.sol
@@ -1,89 +1,122 @@
 pragma solidity ^0.4.19;
-import "browser/token.sol";
-contract userToken  {
-     uint id;
-    address owner;
-    uint amount;
-    uint counter;
-    
-    modifier restricted() {
-        if(msg.sender==owner)_;
-    }
-    function userToken() {
-        owner= msg.sender;
-    }
-    
-    struct concept{
-        string num;
-        string street;
-        string city;
-        string country;
-        string postal_code;
-    }
-    struct user{
-        
-         bool isdoctor;
-         string username;
-         string first_name;
-         string last_name;
-         string password;
-         string email;
-         medical_record medical_info;
-         string dob;
-         string practioner_id;
-         concept addr;
-    }
-    user u;
-    struct medical_record{
-        string id;//unique  id of the user 128 bit
-        string fileHash;//hash last updated file
-        uint vers;  //increment every time file is updated
-        string pointer;//points towards the place where file is stored outside of the block chain
+import "browser/Token.sol";
+import "browser/ERC20.sol";
+import "browser/ERC223.sol";
+import "browser/ERC223ReceivingContract.sol";
+contract userToken is Token(10, 1000), ERC20, ERC223{
+    uint8 id;
+    address _owner;
+    uint8 amount;
+    uint8 counter;
+     bool isdoctor;
+     bytes32 _username;
+     bytes32 first_name;
+     bytes32 last_name;
+     bytes32 password;
+     bytes32 email;
+     bytes32 dob;
+     bytes32 practioner_id;
+     string fileHash;
+     bytes32 addr;
 
-    }
-    mapping (uint => user) users;
     
-    function setdoctor(bool isdoctor){
+    function userToken(){
+        _owner = msg.sender;
+    }
+    
+     
+    
+    event IDD(bytes32 _username,bytes32 first_name,bytes32 last_name,bytes32 password,bytes32 practioner_id,bytes32 email,bytes32 dob,bytes32 addr);
+    event hash(string filehash);
+  function file(string filehash){
+      fileHash=filehash;
+      hash(filehash);
+  }
+    function setIDD(bytes32 username,bytes32 _first_name,bytes32 _last_name,bytes32 _password,bytes32 practioner_id,bytes32 _email,bytes32 _dob,bytes32 addr_) returns (uint id) { 
+        id= counter++; 
+        _username=username;
+        first_name=_first_name;
+        practioner_id=practioner_id;
+        last_name=_last_name;
+        password=_password;
+        email=_email;
+        dob=_dob;
+        addr=addr_;
+        IDD(username,_first_name,_last_name,_password,practioner_id,_email,_dob,addr_);
+        return id;
+    }
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply;
+    }
+    
+    function balanceOf(address _addr) public constant returns (uint) {
+        return _balanceOf[_addr];
+    }
+
+
+	function setdoctor(bool isdoctor){
         isdoctor=true;
     }
-    function set(uint x){
-        counter=x;
+
+    function transfer(address _to, uint _value) public returns (bool) {
+        if (_value > 0 && 
+            _value <= _balanceOf[_owner] &&
+            !isContract(_to) && msg.sender!=_owner) {
+            _balanceOf[_owner] -= _value;
+            _balanceOf[_to] += _value;
+            Transfer(_owner, _to, _value);
+            return true;
+        }
+        return false;
     }
-    function get() returns (uint){
-        return counter;
+
+    function transfer(address _to, uint _value, bytes _data) public returns (bool) {
+        if (_value > 0 && 
+            _value <= _balanceOf[_owner] &&
+            isContract(_to) && msg.sender!=_owner) {
+            _balanceOf[_owner] -= _value;
+            _balanceOf[_to] += _value;
+            ERC223ReceivingContract _contract = ERC223ReceivingContract(_to);
+                _contract.tokenFallback(msg.sender, _value, _data);
+            Transfer(_owner, _to, _value, _data);
+            return true;
+        }
+        return false;
+    }
+
+    function isContract(address _addr) returns (bool) {
+        uint codeSize;
+        assembly {
+            codeSize := extcodesize(_addr)
+        }
+        return codeSize > 0;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
+        if (_allowances[_from][msg.sender] > 0 &&
+            _value > 0 &&
+            _allowances[_from][msg.sender] >= _value &&
+            _balanceOf[_from] >= _value) {
+            _balanceOf[_from] -= _value;
+            _balanceOf[_to] += _value;
+            _allowances[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        }
+        return false;
     }
     
-    event ID(string username,string first_name,string last_name,string password,string email,medical_record medical_info,string dob,concept addr);
-    event IDD(string username,string first_name,string last_name,string password,string practioner_id,string email,medical_record medical_info,string dob,concept addr);
-    function setID(string username,string first_name,string last_name,string password,string email,medical_record medical_info,string dob,concept addr) returns (uint id) { 
-        id= counter++; 
-        set(id+1);
-        u=users[id];
-        u.username=username;
-        u.first_name=first_name;
-        u.last_name=last_name;
-        u.password=password;
-        u.email=email;
-        ID(username,first_name,last_name,password,email,medical_info,dob,addr);
-        return id;
+    function approve(address _spender, uint _value) public returns (bool) {
+        _allowances[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
     }
     
-    function setIDD(string username,string first_name,string last_name,string password,string practioner_id,string email,medical_record medical_info,string dob,concept addr) returns (uint id) { 
-        id= counter++; 
-        set(id+1);
-         u=users[id];
-        u.username=username;
-        u.first_name=first_name;
-        u.practioner_id=practioner_id;
-        u.last_name=last_name;
-        u.password=password;
-        u.email=email;
-        IDD(username,first_name,last_name,password,practioner_id,email,medical_info,dob,addr);
-        return id;
+    function allowance(address _owner, address _spender) public constant returns (uint) {
+        return _allowances[_owner][_spender];
     }
-    function pass()
-    {
-        token(owner);
-    }
+    
+    
     
 }
+
